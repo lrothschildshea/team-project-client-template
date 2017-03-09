0e20238386a912ae407ac5601a1dd388527180d2
Liam Rothschild-Shea <lrothschilds@umass.edu>
2017-03-09 16:30:16 -0500

    setup for startup 3 and removal of unneeded band settings page

--- app/app.js
+++ app/app.js
@@ -1,1 +1,20 @@
-/* Empty for now. :) */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Each major browser view user interface must be imported.
+import UI01 from './components/ui-01.js';
+import UI02 from './components/ui-02.js';
+
+// For each view conditionally determine which view to display
+// depending on if the ID is present in the HTML.
+if (document.getElementById('ui-01') !== null) {
+  ReactDOM.render(
+    <UI01 />,
+    document.getElementById('ui-01')
+  );
+} else if (document.getElementById('ui-02') !== null) {
+  ReactDOM.render(
+    <UI02 />,
+    document.getElementById('ui-02')
+  );
+}
