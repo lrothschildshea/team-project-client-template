83f87e7e063c3dcecb9c5c43c235fb8723da9eb1
spacedome <julien@spacedome.tv>
2017-03-17 23:27:00 -0400

    Add MusicWidget and refactor/parameterize BandPage

--- app/components/BandPage.js
+++ app/components/BandPage.js
@@ -3,106 +3,165 @@ import Navbar from './Navbar.js';
 import BandEdit from './BandEdit.js';
 import EventWidget from './EventWidget.js';
 import Comments from './Comments.js';
+import MusicWidget from './MusicWidget.js';
+
+const band = {
+  name: "Generic Band Name",
+  info: "Music band with instruments",
+  location: "Amherst, MA",
+  memberInfo: "3 Real people are in this band",
+  fans: 420,
+  wanted: [
+    {
+      id: 1,
+      instrument: "Guitarist",
+      info: "Experienced Flamenco guitarist",
+    },
+    {
+      id: 2,
+      instrument: "Saxophone",
+      info: "Play me something spicy",
+    }
+  ],
+  members: [
+    {
+      name: "Sean Morris",
+      id: 1,
+    },
+    {
+      name: "Spongebob",
+      id: 2,
+    },
+    {
+      name: "Squidward",
+      id: 3,
+    },
+  ],
+}
 
 export default class BandPage extends React.Component {
   render() {
     return (
       <div>
-      <Navbar />
-      <BandEdit />
-
-
-      <div className="container band-main">
-        <div className="row band-cover">
-          <div className="band-spacer"></div>
-          <div className="band-name pull-left">
-            <h1>Generic Band Name</h1>
-          </div>
-          <div className="btn-group pull-right" role="group">
-            <button type="button" className="btn btn-default" data-toggle="modal" data-target="#editBandModal">
-              Edit <span className="glyphicon glyphicon-pencil"></span>
-            </button>
-          </div>
-        </div>
-
-        <div className="row">
-
-          <div className="col-md-4 bandpage-left">
-
-              <div className="panel">
-                <div className="panel-heading">
-                  <h3 className="panel-title">Band Info</h3>
-                </div>
-                <div className="panel-body">
-                  <ul className="list-unstyled">
-                    <li><span className="glyphicon glyphicon-map-marker"></span>Amherst, MA </li>
-                    <li><span className="glyphicon glyphicon-info-sign"></span>Music band with instruments </li>
-                    <li><span className="glyphicon glyphicon-user"></span>3 Real people are in this band </li>
-                    <li><span className="glyphicon glyphicon-sunglasses"></span>420 People are fans of Generic Band Name </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="panel">
-                <div className="panel-heading">
-                  <h3 className="panel-title">Wanted</h3>
-                </div>
-                <div className="panel-body">
-                  <ul className="media-list">
-                    <li className="media">
-                      <div className="media-left media-top">
-                        PIC
-                      </div>
-                      <div className="media-body">
-                        <a href="#">Guitarist</a>
-                        <br /> Experienced Flamenco guitarist
-                      </div>
-                      <div className="media-right">
-                        <div className="btn-group" role="group" aria-label="...">
-                          <button type="button" className="btn btn-default">Apply</button>
-                        </div>
-                      </div>
-                    </li>
-                    <li className="media">
-                      <div className="media-left media-top">
-                        PIC
-                      </div>
-                      <div className="media-body">
-                        <a href="#">Saxophone</a>
-                        <br /> Play me something spicy
-                      </div>
-                      <div className="media-right">
-                        <div className="btn-group" role="group" aria-label="...">
-                          <button type="button" className="btn btn-default">Apply</button>
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-
+        <Navbar />
+        <BandEdit band={band} />
+        <div className="container band-main">
+          <BandCover band={band} />
+          <div className="row">
+            <div className="col-md-4 bandpage-left">
+              <BandInfo band={band} />
+              <WantedWidget wanted={band.wanted} />
               <EventWidget />
-
-          </div>
-
-          <div className="col-md-8 bandpage-right">
-
-            <div className="panel">
-              <ul className="nav nav-tabs">
-                <li role="presentation"><a href="#"><span className="glyphicon glyphicon-music"></span></a></li>
-                <li role="presentation" className="active"><a href="#">Soundcloud</a></li>
-                <li role="presentation"><a href="#">Bandcamp</a></li>
-              </ul>
-              <div className="panel-body">
-                <iframe width="100%" height="166" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/293734462&amp;color=ff9944&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
-              </div>
             </div>
-
-            <Comments />
-
+            <div className="col-md-8 bandpage-right">
+              <MusicWidget />
+              <Comments />
+            </div>
           </div>
         </div>
       </div>
+    )
+  }
+}
 
+class BandCover extends React.Component {
+  render() {
+    return (
+      <div className="row band-cover">
+        <div className="band-spacer">
+        </div>
+        <div className="band-name pull-left">
+          <h1>
+            {this.props.band.name}
+          </h1>
+        </div>
+        <div
+          className="btn-group pull-right"
+          role="group">
+          <button
+            type="button"
+            className="btn btn-default"
+            data-toggle="modal"
+            data-target="#editBandModal">
+            Edit <span className="glyphicon glyphicon-pencil"></span>
+        </button>
+      </div>
+    </div>
+    )
+  }
+}
+
+class BandInfo extends React.Component {
+  render() {
+    return (
+      <div className="panel">
+        <div className="panel-heading">
+          <h3 className="panel-title">
+            Band Info
+          </h3>
+        </div>
+        <div className="panel-body">
+          <ul className="list-unstyled">
+            <li>
+              <span className="glyphicon glyphicon-map-marker">
+              </span>
+              {this.props.band.location}
+            </li>
+            <li>
+              <span className="glyphicon glyphicon-info-sign">
+              </span>
+              {this.props.band.info}
+            </li>
+            <li>
+              <span className="glyphicon glyphicon-user">
+              </span>
+              {this.props.band.memberInfo}
+            </li>
+            <li>
+              <span className="glyphicon glyphicon-sunglasses">
+              </span>
+              {this.props.band.fans} People are fans of Generic Band Name
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+class WantedWidget extends React.Component {
+  render() {
+    return (
+      <div className="panel">
+        <div className="panel-heading">
+          <h3 className="panel-title">Wanted</h3>
+        </div>
+        <div className="panel-body">
+          <ul className="media-list">
+            {this.props.wanted.map((want) =>
+              <li key={want.id} className="media">
+                <div className="media-left media-top">
+                  PIC
+                </div>
+                <div className="media-body">
+                  <a href="#">
+                    {want.instrument}
+                  </a>
+                  <br />
+                  {want.info}
+                </div>
+                <div className="media-right">
+                  <div
+                    className="btn-group"
+                    role="group"
+                    aria-label="...">
+                    <button type="button" className="btn btn-default">Apply</button>
+                  </div>
+                </div>
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
     )
   }
